66cc9b34b2aa264519ccc9ebdb1e2811f80cc68c
aiprojectchiwa <140908266+aiprojectchiwa@users.noreply.github.com>
2023-09-11 00:57:49 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import gradio as gr
+import demucs
+
+# Fungsi untuk memproses audio menggunakan Demucs
+def process_audio(audio_file):
+    # Muat model Demucs
+    model = demucs.load_model()
+
+    # Baca audio dari file yang diunggah
+    audio = gr.inputs.Audio(audio_file)
+
+    # Proses audio menggunakan model Demucs
+    separated_audio = model.separate(audio)
+
+    return separated_audio
+
+# Membuat antarmuka Gradio
+iface = gr.Interface(
+    fn=process_audio,
+    inputs="audio",
+    outputs=gr.outputs.Audio(label="Audio Terpisah"),
+    live=True,
+    title="Demucs Audio Separation",
+    description="Pisahkan sumber audio dengan Demucs."
+)
+
+# Jalankan antarmuka Gradio
+if __name__ == "__main__":
+    iface.launch()
