d1dd01c2fb923406b748fec2fda3726b26c1404c
aiprojectchiwa <140908266+aiprojectchiwa@users.noreply.github.com>
2023-09-11 01:29:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,43 @@
-import os
 import gradio as gr
-from scipy.io.wavfile import write
+import demucs
+import torch
+import tempfile
+import os
 
+# Muat model Demucs
+model = demucs.load_model()
 
-def inference(audio):
-  os.makedirs("out", exist_ok=True)
-  write('test.wav', audio[0], audio[1])
-  os.system("python3 -m demucs.separate -n mdx_extra_q -d cpu test.wav -o out")
-  return "./out/mdx_extra_q/test/vocals.wav","./out/mdx_extra_q/test/bass.wav",\
-"./out/mdx_extra_q/test/drums.wav","./out/mdx_extra_q/test/other.wav"
+# Fungsi untuk memproses audio menggunakan Demucs
+def process_audio(audio_file):
+    audio = gr.inputs.Audio(audio_file)
+    
+    # Proses audio menggunakan model Demucs
+    separated_audio = model.separate(audio)
+    
+    return {
+        "Vocal": separated_audio["vocals"].to("cpu"),
+        "Instrumental": separated_audio["accompaniment"].to("cpu")
+    }
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--api', action="store_true", default=False)
-parser.add_argument("--share", action="store_true", default=False, help="share gradio app")
-  
-title = "Demucs"
-description = "Gradio demo for Demucs: Music Source Separation in the Waveform Domain. To use it, simply upload your audio, or click one of the examples to load them. Read more at the links below."
-article = "<p style='text-align: center'><a href='https://arxiv.org/abs/1911.13254' target='_blank'>Music Source Separation in the Waveform Domain</a> | <a href='https://github.com/facebookresearch/demucs' target='_blank'>Github Repo</a></p>"
+# Fungsi untuk menyimpan audio ke file sementara
+def save_audio_to_tempfile(audio):
+    _, temp_file_path = tempfile.mkstemp(suffix=".wav")
+    audio.save(temp_file_path)
+    return temp_file_path
 
-examples=[['test.mp3']]
-gr.Interface(
-    inference, 
-    gr.inputs.Audio(type="numpy", label="Input"), 
-    [gr.outputs.Audio(type="filepath", label="Vocals"),gr.outputs.Audio(type="filepath", label="Bass"),gr.outputs.Audio(type="filepath", label="Drums"),gr.outputs.Audio(type="filepath", label="Other")],
-    title=title,
-    description=description,
-    article=article,
-    examples=examples
-    ).launch(enable_queue=True)
+# Interface untuk mengunggah dan memproses audio
+iface = gr.Interface(
+    fn=process_audio,
+    inputs=gr.inputs.Audio(label="Unggah Audio"),
+    outputs=[
+        gr.outputs.Audio(label="Vocal"),
+        gr.outputs.Audio(label="Instrumental")
+    ],
+    live=True,
+    title="Demucs Audio Separation",
+    description="Unggah audio dan pisahkan vokal dan instrumental dengan Demucs."
+)
+
+# Jalankan antarmuka Gradio
+if __name__ == "__main__":
+    iface.launch()
